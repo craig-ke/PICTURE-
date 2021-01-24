3965664ff92f4b50a630653c9b57993471882215
craig-ke <lawrence.olonyi.67@gmail.com>
2021-01-24 14:37:00 +0300

    add files

---
+++ app/src/main/java/com/craigke/picflic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.craigke.picflic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
