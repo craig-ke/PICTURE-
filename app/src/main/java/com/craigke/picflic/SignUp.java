4df35c846a642ad1e4e999c3687d68c2fdaf1d0c
craig-ke <lawrence.olonyi.67@gmail.com>
2021-01-25 09:42:55 +0300

    add logout

--- app/src/main/java/com/craigke/picflic/SignUp.java
+++ app/src/main/java/com/craigke/picflic/SignUp.java
@@ -1,14 +1,106 @@
 package com.craigke.picflic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = SignUp.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+
+        @BindView(R.id.signup_button)
+        Button mCreateUserButton;
+        @BindView(R.id.Person_Name)
+        EditText mNameEditText;
+        @BindView(R.id.edit_Email) EditText mEmailEditText;
+        @BindView(R.id.editTextPassword2) EditText mPasswordEditText;
+        @BindView(R.id.editPassword3) EditText mConfirmPasswordEditText;
+        @BindView(R.id.loginTextViewtextView)
+        TextView mLoginTextView;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_sign_up);
+            ButterKnife.bind(this);
+            createAuthStateListener();
+
+            mLoginTextView.setOnClickListener((View.OnClickListener) this);
+            mCreateUserButton.setOnClickListener((View.OnClickListener) this);
+            mAuth = FirebaseAuth.getInstance();
+        }
+
+    private void createAuthStateListener() {
+        FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(SignUp.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        public void onClick(View view) {
+
+            if (view == mLoginTextView) {
+                Intent intent = new Intent(SignUp.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+
+            if (view == mCreateUserButton) {
+                createNewUser();
+            }
+
+        }
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 }
+
